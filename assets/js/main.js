326ced5655cb807a6dc3ea731f337145a9220362
steomi <step4novmi@gmail.com>
2021-12-04 20:12:41 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+console.clear();
+/**/
+setTimeout(function () {
+  document.querySelector(".splash").classList.add("seen");
+}, 3000);
+/**/
---
+++ assets/js/slider.js
@@ -0,0 +1,8 @@
+$(".slider").owlCarousel({
+  items: 3,
+  singleItem: true,
+  nav: true,
+  dots: false,
+  loop: true,
+  autoPlay: 3000,
+});
